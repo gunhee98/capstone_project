e79a027dd51bf4d7eb89dff80ad907c9af462eff
YongYong21 <imkinggosu@gmail.com>
2023-05-06 00:12:16 +0900

    adding-nav
    adding-nav

--- project/src/Main.js
+++ project/src/Main.js
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { API_URL, API_KEY, IMAGE_BASE_URL } from "./Config";
 import Grid from "./GridCards.js";
+import "./nomalize.css";
+import TopNav from "./TopNav.js";
 
 function Main() {
-
-
   return (
     <div>
-
-        <Grid></Grid>
-
+      {/* <TopNav></TopNav> */}
+      <Grid></Grid>
     </div>
   );
 }
---
+++ project/src/TopNav.js
@@ -0,0 +1,17 @@
+
+import "./TopNav.css";
+import './nomalize.css';
+
+function TopNav() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Movie</li>
+        <li>Favorite</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default TopNav;
