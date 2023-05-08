11bd1588f91049c8625327fe8707196b26ae6c53
YongYong21 <imkinggosu@gmail.com>
2023-05-08 12:24:28 +0900

    Nav구현
    Nav구현

--- project/src/Main.js
+++ project/src/Main.js
@@ -3,12 +3,13 @@ import { API_URL, API_KEY, IMAGE_BASE_URL } from "./Config";
 import Grid from "./GridCards.js";
 import "./nomalize.css";
 import TopNav from "./TopNav.js";
-
+import './TopNav.css'
 function Main() {
   return (
     <div>
-      {/* <TopNav></TopNav> */}
-      <Grid></Grid>
+      <TopNav></TopNav>
+      <div className="box"></div>
+      {/* <Grid></Grid> */}
     </div>
   );
 }
--- project/src/TopNav.js
+++ project/src/TopNav.js
@@ -1,16 +1,29 @@
-
 import "./TopNav.css";
-import './nomalize.css';
+import "./nomalize.css";
+import React, { useEffect, useState, useRef } from "react";
 
 function TopNav() {
+
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+
+  const updateScroll = () => {
+    setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", updateScroll);
+  }, []);
+
   return (
-    <nav>
+    <nav className={scrollPosition > 100 ? "nav-scrooled" : "nav-scrooled-reverse"} >
       <ul>
         <li>Home</li>
         <li>Movie</li>
         <li>Favorite</li>
       </ul>
     </nav>
+    
   );
 }
 
